b5f115fe077d0f9c2510f102823415dc62bba8c5
Desiree Mendes <desireefrost@live.ca>
2022-02-03 13:16:19 -0500

    Functionality for creating and deleting goals

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,42 @@
 import './App.css';
 import Header from './Components/Header';
-import {Routes, Route} from "react-router-dom";
-import Register from './Register';
+import { useState } from 'react';
+import CreateGoal from './Components/CreateGoal';
+import Goal from './Components/Goal';
+// import {Routes, Route} from "react-router-dom";
+// import Register from './Components/Register';
 
 function App() {
+  const [goals, setGoals] = useState([]);
+
+  function addGoal(newGoal) {
+    setGoals((prevGoals) => {
+      return [...prevGoals, newGoal];
+    });
+  }
+
+  function deleteGoal(id) {
+    setGoals((prevGoals) => {
+      return prevGoals.filter((goalItem, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
-    <div className="App">
+    <div>
       <Header />
-      <Routes>
-      <Route path="/register" element={<Register />} />
-      </Routes>
+      <CreateGoal onAdd={addGoal} />
+      {goals.map((goalItem, index) => {
+        return (
+          <Goal
+            key={index}
+            id={index}
+            title={goalItem.title}
+            content={goalItem.content}
+            onDelete={deleteGoal}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ frontend/src/Components/CreateGoal.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+function CreateGoal(props) {
+  const [goal, setGoal] = useState({
+        title: "",
+        content: ""
+        });
+        
+
+    function handleChange(event) {
+      const { name, value } = event.target;
+
+      setGoal(prevGoal => {
+        return {
+          ...prevGoal,
+          [name]: value
+        }
+      });
+    }
+
+    function submitGoal(event) {
+    props.onAdd(goal)
+    setGoal({
+      title: "",
+      content: ""
+    })
+    event.preventDefault()
+    }
+
+  return (
+    <div>
+      <form className="create-goal">
+        <input name="title" 
+        onChange={handleChange}
+        value={goal.title}
+        placeholder="Title" 
+        />
+        <textarea 
+        name="content" 
+        onChange={handleChange}
+        value={goal.content}
+        placeholder="What's your goal?" rows="3" />
+        <button onClick={submitGoal}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateGoal;
