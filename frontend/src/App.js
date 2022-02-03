c44a84c38d1b4584bad2f20dc351a0cc12ce0209
Desiree Mendes <desireefrost@live.ca>
2022-02-03 12:12:58 -0500

    register path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './Components/Header';
+import {Routes, Route} from "react-router-dom";
+import Register from './Register';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+      <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
