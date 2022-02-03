9714976566161b751736d07a1a03d4d64548cc74
Desiree Mendes <desireefrost@live.ca>
2022-02-03 12:04:10 -0500

    Create header, cleanup app

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,8 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
-const db = require('./db');
-const dbHelpers = require('./helpers/dbHelpers')(db);
+// const db = require('./db');
+// const dbHelpers = require('./Helpers/dbHelpers')(db);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,10 +17,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/users', usersRouter(dbHelpers));
+// app.use('/api/users', usersRouter(dbHelpers));
 
 module.exports = app;
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
